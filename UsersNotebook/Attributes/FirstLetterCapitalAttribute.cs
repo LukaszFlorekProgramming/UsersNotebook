9b67e395c866c9920224af8d0814cc9dc23377ff
Lukasz Florek <lukaszflorek2@gmail.com>
2023-07-12 17:21:46 +0200

    adding FirstLetterCapitalAttribute

---
+++ UsersNotebook/Attributes/FirstLetterCapitalAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersNotebook.Attributes
+{
+    public class FirstLetterCapitalAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && value is string name)
+            {
+                if (name.Length > 0 && char.IsLower(name[0]))
+                {
+                    return new ValidationResult("Pierwsza litera powinna być napisana z dużej litery.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- UsersNotebook/Core/Models/Domains/User.cs
+++ UsersNotebook/Core/Models/Domains/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using UsersNotebook.Attributes;
 
 namespace UsersNotebook.Core.Models.Domains
 {
@@ -8,10 +9,12 @@ namespace UsersNotebook.Core.Models.Domains
         [Required(ErrorMessage = "Pole Imię jest wymagane.")]
         [MaxLength(50)]
         [RegularExpression(@"^[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+$", ErrorMessage = "Imię może zawierać tylko litery.")]
+        [FirstLetterCapital(ErrorMessage = "Pierwsza litera powinna być napisana z dużej litery.")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Pole Nazwisko jest wymagane.")]
         [MaxLength(150)]
         [RegularExpression(@"^[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+$", ErrorMessage = "Imię może zawierać tylko litery.")]
+        [FirstLetterCapital(ErrorMessage = "Pierwsza litera powinna być napisana z dużej litery.")]
         public string Surname { get; set; }
         [Required(ErrorMessage = "Pole Data Urodzenia jest wymagane.")]
         public DateTime DateOfBirth { get; set; }
